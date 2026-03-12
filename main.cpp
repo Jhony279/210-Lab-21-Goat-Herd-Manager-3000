18274ad7817f9a9e447b8c9c7d73feae89bfd6b7
Jhony279 <lordjhony732@gmail.comm>
2026-03-11 21:51:28 -0700

    Creating goat class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 using namespace std;
 
+const int 
+
+class Goat {
+    private:
+        int age;
+        string name;
+        string color;
+        string names[15] = {"Billy", "Nanny", "Gruff", "Baa", "Goaty", "Capra", "Cheese", "Horny", "Bleat", "Moo", "Fuzzy", "Woolly", "Cloven", "Pygmy", "Alpine"};
+        string colors[15] = {"White", "Black", "Brown", "Gray", "Violet", "Orange", "Golden", "Silver", "Cream", "Tan", "Red", "Blue", "Green", "Yellow", "Pink"};
+    public:
+};
+
 /**
  * @brief - Description of main
  * @return 
