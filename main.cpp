fc6543a05fcfe7700dee589a797139b201f29cc5
Jhony279 <lordjhony732@gmail.comm>
2026-03-11 22:01:35 -0700

    Added constructors to the goat class

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 #include <iostream>
 using namespace std;
 
-const int 
-
 class Goat {
     private:
         int age;
         string name;
         string color;
-        string names[15] = {"Billy", "Nanny", "Gruff", "Baa", "Goaty", "Capra", "Cheese", "Horny", "Bleat", "Moo", "Fuzzy", "Woolly", "Cloven", "Pygmy", "Alpine"};
-        string colors[15] = {"White", "Black", "Brown", "Gray", "Violet", "Orange", "Golden", "Silver", "Cream", "Tan", "Red", "Blue", "Green", "Yellow", "Pink"};
+        string names[15] = {"Billy", "Nanny", "Gruff", "Baa", "Goaty", "Capra", "Cheese"
+            , "Horny", "Bleat", "Moo", "Fuzzy", "Woolly", "Cloven", "Pygmy", "Alpine"};
+        string colors[15] = {"White", "Black", "Brown", "Gray", "Violet", "Orange"
+            , "Golden", "Silver", "Cream", "Tan", "Red", "Blue", "Green", "Yellow", "Pink"};
     public:
+        Goat(){
+            age = rand() % 20 + 1; // Random age between 1 and 20
+            name = names[rand() % 15]; // Random name from the list
+            color = colors[rand() % 15]; // Random color from the list
+        }
+
+        Goat(int a, string n, string c) {
+            age = a;
+            name = n;
+            color = c;
+        }
 };
 
 /**
